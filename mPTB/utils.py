682055154a4913495d53a4b65784a810141abb78
to-aoki <toshihiko.aoki@gmail.com>
2019-04-22 02:18:24 +0900

    add Timer class

--- mPTB/utils.py
+++ mPTB/utils.py
@@ -16,11 +16,28 @@
 """Utility Functions"""
 
 import os
+import time
 import random
 import logging
 import numpy as np
 import torch
 from .preprocessing import *
+
+
+class Timer(object):
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+
+    def __enter__(self):
+        self.start = time.time()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.time()
+        self.secs = self.end - self.start
+        self.msecs = self.secs * 1000  # millisecs
+        if self.verbose:
+            print('elapsed time: %f ms' % self.msecs)
 
 
 def to_bert_ids(
