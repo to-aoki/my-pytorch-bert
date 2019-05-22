ae6c8d6af759798f0fb02fd39347b6e55aa4ca57
to-aoki <toshihiko.aoki@gmail.com>
2019-05-22 09:55:18 +0900

    fix

--- train-sentencepiece.py
+++ train-sentencepiece.py
@@ -1,6 +1,20 @@
 # This file is based on https://github.com/yoheikikuta/bert-japanese/blob/master/src/train-sentencepiece.py.
 # Fix to read the json-file to make the setting method same as other code.
-"""Bert pre-training."""
+#
+# Author : Yohei Kikuta
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Sentencepiece training."""
 
 import json
 import glob
